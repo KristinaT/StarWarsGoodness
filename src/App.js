20679e625f4641bbbc11aad4d25a70e60228a0f2
Kristina Taneva <Kristina.Taneva@endava.com>
2018-11-06 12:55:08 +0100

    Testing Carousel

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import SearchBar from './Components/SearchBar';
 import SearchBarDetails from './Components/SearchBarDetails';
 import * as Constants from './Constants/Constants';
 import './Styles/App.css';
+import Image from './Images/slide_1.png'
+import Image2 from './Images/slide_2.png'
+import styles from 'react-responsive-carousel/lib/styles/carousel.min.css';
+var Carousel = require('react-responsive-carousel').Carousel;
 
 class App extends Component {
 
@@ -92,15 +97,15 @@ class App extends Component {
       console.log(e);
     }
 
+    const arr = [Image, Image2]
   }
 
-  // TODO: Add loader!
   render() {
     const { searchedItemDetails, foundDetails, loading } = this.state;
 
     return (
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
           * Star Wars Goodness *
             <SearchBar
             inputElement={this.inputElement}
@@ -113,7 +118,35 @@ class App extends Component {
             showSearchBarDetails={foundDetails}
             loading = {loading}
           />
-        </header>
+          
+        </header> */}
+        {/* <Carousel
+        autoPlay={2000}
+        animationSpeed={1000}
+        slides= {this.arr}
+        >
+            <img src={Image} alt="bla" height="242" width="242" />
+            <img src={Image2} alt="ble" height="242" width="242" />
+        </Carousel> */}
+        <Carousel
+        infiniteLoop={true}
+        autoPlay={true}
+        interval='2000'
+        showArrows={false}
+        showIndicators={false}
+        useKeyboardArrows={true}
+        dynamicHeight={true}
+        showThumbs={false}
+        transitionTime='1000'
+        >
+          <div>
+              <img src={Image} />
+              
+          </div>
+          <div>
+              <img src={Image2} />
+          </div>
+        </Carousel>
       </div>
     );
   }
