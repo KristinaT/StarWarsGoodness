92a4a3e274741f3f33a7f7128f6673b06e36d686
Kristina Taneva <Kristina.Taneva@endava.com>
2018-11-07 14:06:23 +0100

    Added carousel

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import SearchBar from './Components/SearchBar';
 import SearchBarDetails from './Components/SearchBarDetails';
 import * as Constants from './Constants/Constants';
 import './Styles/App.css';
-import Image from './Images/slide_1.png'
-import Image2 from './Images/slide_2.png'
+import Image from './Images/slide_1.jpeg'
+import Image2 from './Images/slide_2.jpg'
+import Image3 from './Images/slide_3.jpg'
 import styles from 'react-responsive-carousel/lib/styles/carousel.min.css';
 var Carousel = require('react-responsive-carousel').Carousel;
 
@@ -96,8 +96,6 @@ class App extends Component {
     catch (e) {
       console.log(e);
     }
-
-    const arr = [Image, Image2]
   }
 
   render() {
@@ -105,7 +103,19 @@ class App extends Component {
 
     return (
       <div className="App">
-        {/* <header className="App-header">
+        <header className="App-header">
+        <Carousel
+          autoPlay={true}
+          infiniteLoop={true}
+          animationSpeed={1000}
+          showThumbs={false}
+          showIndicators={false}
+          showArrows={false}
+          >
+            <img src={Image} alt="bla" height="550" width="600" />
+            <img src={Image2} alt="ble" height="550" width="600" />
+            <img src={Image3} alt="blu" height="550" width="600" />
+        </Carousel> 
           * Star Wars Goodness *
             <SearchBar
             inputElement={this.inputElement}
@@ -118,35 +128,10 @@ class App extends Component {
             showSearchBarDetails={foundDetails}
             loading = {loading}
           />
-          
-        </header> */}
-        {/* <Carousel
-        autoPlay={2000}
-        animationSpeed={1000}
-        slides= {this.arr}
-        >
-            <img src={Image} alt="bla" height="242" width="242" />
-            <img src={Image2} alt="ble" height="242" width="242" />
-        </Carousel> */}
-        <Carousel
-        infiniteLoop={true}
-        autoPlay={true}
-        interval='2000'
-        showArrows={false}
-        showIndicators={false}
-        useKeyboardArrows={true}
-        dynamicHeight={true}
-        showThumbs={false}
-        transitionTime='1000'
-        >
-          <div>
-              <img src={Image} />
-              
-          </div>
-          <div>
-              <img src={Image2} />
-          </div>
-        </Carousel>
+     
+        </header>
+
+
       </div>
     );
   }
